3b93e4d00f59e18ad4c0e95d2647a26b37257b50
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-19 15:11:01 -0400

    have integration tests ready

--- instructor/BusinessClockSolution/BusinessClockApi/Program.cs
+++ instructor/BusinessClockSolution/BusinessClockApi/Program.cs
@@ -1,3 +1,8 @@
+
+
+using BusinessClockApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,7 +21,11 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/status", () =>
 {
-    var response = new { open = true };
+    var response = new GetStatusResponse
+    {
+        Open = true,
+       
+    };
     return Results.Ok(response);
 });
 
