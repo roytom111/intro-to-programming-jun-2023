259c9c738754ff615ec8307d6b86ab96abe33ed5
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-19 16:33:41 -0400

    done with monday

---
+++ instructor/BusinessClockSolution/BusinessClockApi/Services/BusinessClockService.cs
@@ -0,0 +1,33 @@
+﻿using BusinessClockApi.Models;
+
+namespace BusinessClockApi.Services;
+
+public class BusinessClockService
+{
+    private readonly ISystemTime _systemTime;
+
+    public BusinessClockService(ISystemTime systemTime)
+    {
+        _systemTime = systemTime;
+    }
+
+    public GetStatusResponse GetCurrentStatus()
+    {
+        DateTime now = _systemTime.GetCurrent();
+        bool isOpen = now.DayOfWeek != DayOfWeek.Sunday && now.DayOfWeek != DayOfWeek.Saturday;
+        return new GetStatusResponse { Open = isOpen };
+    }
+}
+
+public interface ISystemTime
+{
+    DateTime GetCurrent();
+}
+
+public class SystemTime : ISystemTime
+{
+    public DateTime GetCurrent()
+    {
+        return DateTime.Now;
+    }
+}
