ada5259e6e8c589414b1b2a55892b3150b7fa465
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-21 11:06:52 -0400

    before break wednesday

---
+++ instructor/GamesSolution/Games/BowlingGame.cs
@@ -0,0 +1,22 @@
+﻿namespace Games;
+
+public class BowlingGame
+{
+
+    private readonly Dictionary<string, int> _scores = new();
+    
+    public void AddPlayer(string name, int score)
+    {
+
+        if(_scores.ContainsKey(name) )
+        {
+            throw new PlayerAlreadyAddedToGameException();
+        } else
+        {
+            _scores.Add(name, score);
+        }
+       // store some kind of list of players and their scores
+       // unless a player with that same name already exists.
+       // in that case, punch them in the nose.
+    }
+}
