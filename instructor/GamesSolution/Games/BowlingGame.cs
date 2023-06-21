b5a62cfc84f9c0fe26f464e5a3647ce3fe5b1a27
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-21 12:33:03 -0400

    really done

--- instructor/GamesSolution/Games/BowlingGame.cs
+++ instructor/GamesSolution/Games/BowlingGame.cs
@@ -7,17 +7,21 @@ public class BowlingGame
     
     public void AddPlayer(string name, int score)
     {
-       
-        if (_players.Any(p => p.Name.Trim().ToLowerInvariant() == name.Trim().ToLowerInvariant()))
+
+        if (PlayerExists(name))
         {
             throw new PlayerAlreadyAddedToGameException();
-        } else
+        }
+        else
         {
             _players.Add(new Player(name, score));
         }
-       // store some kind of list of players and their scores
-       // unless a player with that same name already exists.
-       // in that case, punch them in the nose.
+
+    }
+
+    private bool PlayerExists(string name)
+    {
+        return _players.Any(p => p.Name.Trim().ToLowerInvariant() == name.Trim().ToLowerInvariant());
     }
 }
 
