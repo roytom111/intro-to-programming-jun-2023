aff55a33e0962b5ef00c8b031ad7c5bb368a2507
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-21 16:32:07 -0400

    done

--- instructor/GamesSolution/Games/BowlingGame.cs
+++ instructor/GamesSolution/Games/BowlingGame.cs
@@ -1,43 +1,11 @@
 ﻿namespace Games;
 
-public class BowlingGame
+public class BowlingGame : Game
 {
-    // "Primitive Obsession"
-    private readonly List<Player> _players = new(); // intention revealing than Dictionary<string, int>
-    
-    public void AddPlayer(string name, int score)
-    {
-        GuardForValidScore(score);
-        GuardForPlayerAlreadyExisting(name);
-
-        _players.Add(new Player(name, score));
-
-
-    }
-
-    private void GuardForPlayerAlreadyExisting(string name)
-    {
-        if (PlayerExists(name))
-        {
-            throw new PlayerAlreadyAddedToGameException();
-        }
-    }
-
-    private static void GuardForValidScore(int score)
+    protected override void GuardForValidScore(int score)
     {
         if (score < 0 || score > 300) { throw new InvalidBowlingScoreException(); }
     }
-
-    private bool PlayerExists(string name)
-    {
-        return _players.Any(p => p.Name.Trim().ToLowerInvariant() == name.Trim().ToLowerInvariant());
-    }
-
-    internal List<Player> GetPlayers()
-    {
-        return _players;
-    }
-  
 }
 
 public record Player(string Name, int score);
--- instructor/GamesSolution/Games/GameScorer.cs
+++ instructor/GamesSolution/Games/GameScorer.cs
@@ -3,6 +3,24 @@
 public class GameScorer
 {
 
+    // Overloading
+    public ScoreReport GenerateScoreReportFor(GolfGame game)
+    {
+        GuardAgainstInvalidGame(game);
+        var players = game.GetPlayers();
+
+        int highScore = players.Max(p => p.score);
+        int lowScore = players.Min(p => p.score);
+
+
+        return new ScoreReport
+        {
+            Winners = players.Where(p => p.score == lowScore).ToList(),
+            Losers = highScore == lowScore ? new() : players.Where(p => p.score == highScore).ToList(),
+            Average = players.Average(p => p.score)
+        };
+    }
+
     public ScoreReport GenerateScoreReportFor(BowlingGame game)
     {
         GuardAgainstInvalidGame(game);
@@ -20,7 +38,7 @@ public class GameScorer
         };
     }
 
-    private static void GuardAgainstInvalidGame(BowlingGame game)
+    private static void GuardAgainstInvalidGame(Game game)
     {
         if (game.GetPlayers().Count < 2)
         {
