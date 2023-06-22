9e125eae307d1b0e5603fa8749f87272a561ebc4
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-22 12:35:08 -0400

    starting composition

--- instructor/BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
+++ instructor/BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
@@ -4,9 +4,17 @@
 namespace Banking.UnitTests.BankAccount;
 public class GoldCustomersGetABonusOnDeposits
 {
-    [Fact(Skip = "Working on it")]
+    [Fact()]
     public void BonusIsApplied()
     {
+        var account = new Account();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+        var expectedNewBalance = openingBalance + amountToDeposit + 10M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(expectedNewBalance, account.GetBalance());
 
     }
 }
