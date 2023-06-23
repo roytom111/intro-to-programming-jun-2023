e2c741aa4c4ff231746eb8de561984464cb9bcf2
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-23 15:00:37 -0400

    before refactor

--- instructor/GamesSolution/CSharpStuff/MethodsDetails.cs
+++ instructor/GamesSolution/CSharpStuff/MethodsDetails.cs
@@ -33,13 +33,16 @@ public class MethodsDetails
 
         var answer2 = math.Add(10, 20, 30, 40);
 
+        var extraNumbers = new int[] { 3, 4, 5, 6, 7, 8, 9 };
+        var answer3= math.Add(1, 2, extraNumbers);
+        Assert.Equal(45, answer3);
     }
 }
 
 
 public class MathThing
 {
-    public int Add(int a, int b, params int[] rest)
+    public int Add(int a, int b,  params int[] rest)
     {
         return a + b + rest.Sum();
     }
--- instructor/GreeterPractice/Greeter/GreetingMaker.cs
+++ instructor/GreeterPractice/Greeter/GreetingMaker.cs
@@ -1,14 +1,65 @@
-﻿namespace Greeter
+﻿using System.Runtime.CompilerServices;
+
+namespace Greeter;
+
+public class GreetingMaker
 {
-    public class GreetingMaker
+
+    private readonly IProvideBannedNames _bannedNamesService;
+    private readonly INotifyOfTrolls _trollNotifier;
+
+    public GreetingMaker(IProvideBannedNames bannedNamesService, INotifyOfTrolls trollNotifier)
     {
-        public GreetingMaker()
+        _bannedNamesService = bannedNamesService;
+        _trollNotifier = trollNotifier;
+    }
+
+    public string Greet(params string?[]? names)
+    {
+        if(names is null) { return "Hello, Chief!"; }
+        var bannedNames = _bannedNamesService.GetListOfBannedNames();
+        var attemptedBannedNames = names.Intersect(bannedNames).ToList();
+        if(attemptedBannedNames.Count == names.Length)
         {
+            _trollNotifier.Notify("User Used All Invalid Names: " + string.Join(", ", attemptedBannedNames));
+        }
+        if(attemptedBannedNames.Any())
+        {
+            var ellidedNames = new List<string>();
+            var cleanNames = new List<string>();
+            foreach(var name in names)
+            {
+                if(attemptedBannedNames.Contains(name)) { ellidedNames.Add(StringUtilties.Elide(name)); } else {  cleanNames.Add(name); }
+            }
+            names = cleanNames.Concat(ellidedNames).ToArray();
+        }
+        var allUpper = names.All(n => n.ToUpperInvariant() == n);
+
+        var someUpper = !allUpper && names.Any(n => n.ToUpperInvariant() == n);
+
+        if(someUpper)
+        {
+            var nonShoutedNames = names.Where(n => n.ToUpperInvariant() != n).ToList();
+            var shoutedNames = names.Except(nonShoutedNames);
+            return $"Hello, {string.Join(", ", nonShoutedNames)}, AND {string.Join(", ",shoutedNames)}!";
+        }
+        var prefix = allUpper ? "HELLO" : "Hello";
+        var separator = allUpper ? "AND": "and";
+        if (names.Length > 1)
+        {
+            var beginning = names.Take(names.Length - 1).ToArray();
+            var last = names[names.Length - 1];
+            var startNames = string.Join(", ", beginning);
+
+            return $"{prefix}, {startNames}, {separator} {last}!";
+        } else
+        {
+            return $"{prefix}, {names[0]}!";
         }
 
-        public string Greet(string name)
-        {
-            return "Hello, Windom.";
-        }
+        
+        
     }
+
+   
 }
