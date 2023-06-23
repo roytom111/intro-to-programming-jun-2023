932bebf7d59de5bf6c6cb04146254c3fd4d73e1b
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-23 15:49:19 -0400

    Refactoring

--- instructor/GreeterPractice/Greeter/GreetingMaker.cs
+++ instructor/GreeterPractice/Greeter/GreetingMaker.cs
@@ -16,35 +16,22 @@ public class GreetingMaker
 
     public string Greet(params string?[]? names)
     {
-        if(names is null) { return "Hello, Chief!"; }
+        if (names is null) { return "Hello, Chief!"; }
+        
         var bannedNames = _bannedNamesService.GetListOfBannedNames();
-        var attemptedBannedNames = names.Intersect(bannedNames).ToList();
-        if(attemptedBannedNames.Count == names.Length)
-        {
-            _trollNotifier.Notify("User Used All Invalid Names: " + string.Join(", ", attemptedBannedNames));
-        }
-        if(attemptedBannedNames.Any())
-        {
-            var ellidedNames = new List<string>();
-            var cleanNames = new List<string>();
-            foreach(var name in names)
-            {
-                if(attemptedBannedNames.Contains(name)) { ellidedNames.Add(StringUtilties.Elide(name)); } else {  cleanNames.Add(name); }
-            }
-            names = cleanNames.Concat(ellidedNames).ToArray();
-        }
-        var allUpper = names.All(n => n.ToUpperInvariant() == n);
+        var namesThatAreInTheBannedList = names.Intersect(bannedNames).ToList();
+        CheckAndNotifyOfTrolls(names, namesThatAreInTheBannedList);
+        names = SanitizeNamesFromBannedList(names, namesThatAreInTheBannedList);
+        bool allUpper = IsEveryoneShouting(names);
 
-        var someUpper = !allUpper && names.Any(n => n.ToUpperInvariant() == n);
+        var someUpper = !allUpper && AreSomePeopleShouting(names);
 
-        if(someUpper)
+        if (someUpper)
         {
-            var nonShoutedNames = names.Where(n => n.ToUpperInvariant() != n).ToList();
-            var shoutedNames = names.Except(nonShoutedNames);
-            return $"Hello, {string.Join(", ", nonShoutedNames)}, AND {string.Join(", ",shoutedNames)}!";
+            return DealWithTheShouters(names);
         }
         var prefix = allUpper ? "HELLO" : "Hello";
-        var separator = allUpper ? "AND": "and";
+        var separator = allUpper ? "AND" : "and";
         if (names.Length > 1)
         {
             var beginning = names.Take(names.Length - 1).ToArray();
@@ -52,14 +39,61 @@ public class GreetingMaker
             var startNames = string.Join(", ", beginning);
 
             return $"{prefix}, {startNames}, {separator} {last}!";
-        } else
+        }
+        else
         {
             return $"{prefix}, {names[0]}!";
         }
 
-        
-        
+
+
     }
 
-   
+    private static string DealWithTheShouters(string?[]? names)
+    {
+        var nonShoutedNames = names.Where(n => n.ToUpperInvariant() != n).ToList();
+        var shoutedNames = names.Except(nonShoutedNames);
+        var response = $"Hello, {string.Join(", ", nonShoutedNames)}, AND {string.Join(", ", shoutedNames)}!";
+        return response;
+    }
+
+    private static bool AreSomePeopleShouting(string?[]? names)
+    {
+        return names.Any(n => n.ToUpperInvariant() == n);
+    }
+
+    private static bool IsEveryoneShouting(string?[]? names)
+    {
+        return names.All(n => n.ToUpperInvariant() == n);
+    }
+
+    private static string?[]? SanitizeNamesFromBannedList(string[] names, List<string> attemptedBannedNames)
+    {
+        if (attemptedBannedNames.Any())
+        {
+            var ellidedNames = new List<string>();
+            var cleanNames = new List<string>();
+            foreach (var name in names)
+            {
+                if (attemptedBannedNames.Contains(name)) { 
+                    ellidedNames.Add(StringUtilties.Elide(name)); 
+                } else 
+                { 
+                    cleanNames.Add(name); 
+                }
+            }
+            names = cleanNames.Concat(ellidedNames).ToArray();
+        }
+
+        return names;
+    }
+
+    private void CheckAndNotifyOfTrolls(string?[]? names, List<string?> attemptedBannedNames)
+    {
+        if (attemptedBannedNames.Count == names.Length)
+        {
+            _trollNotifier.Notify("User Used All Invalid Names: " + string.Join(", ", attemptedBannedNames));
+        }
+    }
+
 }
